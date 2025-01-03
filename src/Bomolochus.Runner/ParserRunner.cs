137127a85b90e68367ccd9189d69d2078a596a88
Jason Holloway <miceeatnicerice@hotmail.com>
2025-01-03 19:48:22 +0000

    wip

--- src/Bomolochus.Runner/ParserRunner.cs
+++ src/Bomolochus.Runner/ParserRunner.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using Bomolochus;
 using Bomolochus.Text;
 
 namespace Bomolochus.Runner;
@@ -22,57 +23,15 @@ public static class ParserRunner
     public static Parsed<V> Parse<V>(this IStep<V> parser, ParserOps.ParseContext parseContext)
         where V : Parsable
     {
-        var frames = new Stack<Frame>(
-        [
-            new Frame.RunStep(new RunContext([], [], parseContext), parser)
-        ]);
+        var fibre = new Fibre([], [], parseContext, parser);
+        var frames = new Stack<Fibre>([fibre]);
         
         ContinueLoop:
-        
-        /* A | B is being parsed as a complete expression
-         * which is then happily fed into Conjunction
-         *
-         * but - the strength isn't communicated to the continuation 
-         * if we've parsed A|B, this result is too rich for Conjunction
-         *
-         * Conjunction can consume subexpressions
-         * with only relatively weak composite strengths
-         * eg single values, or additions
-         *
-         * these strengths are accumulated upwards
-         * rather than the power of parsing propagating downwards
-         * we have both directions in play
-         *
-         * composite strengths would be sufficient to work
-         * but they would require us to evaluate all decontextualised possibilities per step
-         * which is plainly wasteful
-         * there must always be a need for a particular parsing
-         * hence, propagation required
-         *
-         * the LHS Exp should itself have a propagated strength
-         * ie an addition can only combine weakly-parsed expressions on both of its legs
-         *
-         * so we start parsing the addition,
-         * but if we constrain it, we can't cache it
-         * given a site can have multiple results
-         * we can filter out too-strong parsings
-         * ie on our continuation, we filter out those that our operator can't contain
-         * this is what we are currently missing
-         *
-         * and as we parse forwards,
-         * we constrain the parsing to only try steps that are possible
-         * caching is still in place, but there is _always_ a strength in play for each site
-         * 
-         * an additional requirement: 
-         * we need to specify strength on both legs then
-         * yet more syntactical detritus
-         * or - not if we use DelimitedList... we get it for free
-         *
-         * so all we need is to read the RequiredStrength of a continuation bind
-         */
 
-        while (TryGetNextStep(out var x, out var step))
+        while (frames.TryPop(out var x))
         {
+            var step = x.Step;
+            
             if (x is { ParseContext.Strength: var strength } 
                 && step.Info.RequiresStrength is int requiredStrength)
             {
@@ -81,10 +40,10 @@ public static class ParserRunner
                     continue;
                 }
             }
-            
+                    
             if (step is ICacheableStep c)
             {
-                if (x.StepCache.TryGetValue(c, out var cell))
+                if (x.Continuations.TryGetValue(c, out var cell))
                 {
                     cell.AddContinuation(next =>
                     {
@@ -95,14 +54,14 @@ public static class ParserRunner
                         
                         foreach (var s in next.Steps)
                         {
-                            frames.Push(new Frame.RunStep(x.Fork(), s));
+                            frames.Push(x.Fork(s));
                         }
                     });
                     
                     goto ContinueLoop;
                 }
             }
-            
+                    
             switch (step)
             {
                 case IBindStep s:
@@ -112,7 +71,7 @@ public static class ParserRunner
                     if (s is ICacheableStep cs)
                     {
                         cell = new ContinuationCell(cs);
-                        x.StepCache[cs] = cell;
+                        x.Continuations[cs] = cell;
                     }
                     
                     var left = s.Left ?? Step.From(false);
@@ -122,7 +81,7 @@ public static class ParserRunner
                         Binds = x.Binds.Push(new BindFrame.Started(s, cell))
                     };
 
-                    frames.Push(new Frame.RunStep(x, left)); //default step fills in when left leg is empty for convenience
+                    frames.Push(x with { Step = left }); //default step fills in when left leg is empty for convenience
 
                     continue;
                 }
@@ -133,7 +92,7 @@ public static class ParserRunner
 
                     if (!split.IsEmpty)
                     {
-                        x = x with { StepCache = [] };
+                        x = x with { Continuations = [] };
                     }
                     
                     var parsed = Parsing.From(s.Value, split);
@@ -191,7 +150,7 @@ public static class ParserRunner
 
                             foreach (var nextStep in next.Steps)
                             {
-                                frames.Push(new Frame.RunStep(x2.Fork(), nextStep));
+                                frames.Push(x2.Fork(nextStep));
                             }
                             
                             break;
@@ -213,54 +172,11 @@ public static class ParserRunner
                 default: throw new NotImplementedException();
             }
         }
-
-        return default!;
         
-
-        bool TryGetNextStep(out RunContext context, out IStep step)
-        {
-            while (frames.TryPop(out var frame))
-            {
-                switch (frame)
-                {
-                    // case Frame.RunSteps(_, []): continue;
-
-                    case Frame.RunStep(var x, var s):
-                    {
-                        context = x;
-                        step = s;
-                        return true;
-                    }
-
-                    // case Frame.RunSteps(var x, [var s, .. var alternatives]):
-                    // {
-                    //     context = x;
-                    //     step = s;
-                    //     frames.Push(new Frame.RunSteps(x.Fork(), alternatives));
-                    //     return true;
-                    // }
-
-                    case Frame.RunContinuations:
-                    {
-                        break;
-                    }
-                }
-            }
-
-            context = default!;
-            step = default!;
-            return false;
-        }
-    }
-    
-    abstract record Frame
-    {
-        public record RunStep(RunContext Context, IStep Step) : Frame;
-        public record RunContinuations() : Frame;
+        return default!;
     }
 
-
-
+    record FibreStep(Fibre Context, IStep Step);
 
 
 
@@ -281,13 +197,15 @@ public static class ParserRunner
         }
     }
 
-    private record RunContext(
+    private record Fibre(
         ImmutableStack<BindFrame> Binds, 
-        Dictionary<ICacheableStep, ContinuationCell> StepCache,
-        ParserOps.ParseContext ParseContext)
+        Dictionary<ICacheableStep, ContinuationCell> Continuations,
+        ParserOps.ParseContext ParseContext,
+        IStep Step
+        )
     {
-        public RunContext Fork()
-            => this with { ParseContext = ParseContext.Fork() };
+        public Fibre Fork(IStep step)
+            => this with { ParseContext = ParseContext.Fork(), Step = step };
 
         public override string ToString() => ParseContext.ToString();
     }
